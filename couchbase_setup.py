81dafb061bbb8ec3bc449bf69d74e60ed6a3d732
carrycooldude <rawatkari554@gmail.com>
2025-03-24 21:57:52 +0530

    Database connection done and add a sample data

---
+++ couchbase_setup.py
@@ -0,0 +1,13 @@
+from couchbase.cluster import Cluster, ClusterOptions
+from couchbase.auth import PasswordAuthenticator
+
+# Connect to Couchbase
+def get_couchbase_cluster():
+    cluster = Cluster("couchbase://localhost", ClusterOptions(PasswordAuthenticator("Administrator", "Administrator")))
+    bucket = cluster.bucket("rag_db")
+    collection = bucket.default_collection()
+    return collection
+
+if __name__ == "__main__":
+    collection = get_couchbase_cluster()
+    print("✅ Couchbase connected!")
